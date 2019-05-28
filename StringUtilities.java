c1ced115d1d4eb27cdac00b9347f222c84ae239b
alilinykh <aililnykh@dtcc.edu>
2019-05-28 10:22:54 -0400

    1020am Strings are done

--- StringUtilities.java
+++ StringUtilities.java
@@ -1,64 +1,74 @@
-
+    
 public class StringUtilities {
-    /**
-     * @param input value to be returned
-     * @return `input`
-     */
-    public String returnInput(String input) {
-        return input;
-    }
-
-    /**
-     * @param baseValue value to be added to
-     * @param valueToBeAdded value to add
-     * @return the concatenation of `baseValue` and `valueToBeAdded`
-     */
-    public String concatenate(String baseValue, String valueToBeAdded) {
-        return baseValue + valueToBeAdded;
-    }
-
-    /**
-     * @param valueToBeReversed value to be reversed
-     * @return identical string with characters in opposite order
-     */
-    public String reverse(String valueToBeReversed) {
-        String reversedValue = "";
-        String[] valueToBeReversedArr = valueToBeReversed.split("");
-        for(int i = 0; i < valueToBeReversedArr.length; i++) {
-            String letter = valueToBeReversedArr[valueToBeReversedArr.length-(i+1)];
-            String newLetter = valueToBeReversedArr[i];
-            newLetter = letter;
-            reversedValue += newLetter;
-            
+        /**
+         * @param input value to be returned
+         * @return `input`
+         */
+        public String returnInput(String input) {
+            return input;
         }
-        
-        return reversedValue;
-    }
-
-    /**
-     * @param word word to get middle character of
-     * @return middle character of `word`
-     */
-    public Character getMiddleCharacter(String word) {
-        String [] wordArr = word.split("");
-        int middle = wordArr.length/2; 
-        return word.charAt(middle) ;
-    }
-
-    /**
-     * @param value value to have character removed from
-     * @param charToRemove character to be removed from `value`
-     * @return `value` with char of value `charToRemove` removed
-     */
-    public String removeCharacter(String value, Character charToRemove) {
-        return null;
-    }
-
-    /**
-     * @param sentence String delimited by spaces representative of a sentence
-     * @return last `word` in sentence
-     */
+    
+        /**
+         * @param baseValue value to be added to
+         * @param valueToBeAdded value to add
+         * @return the concatenation of `baseValue` and `valueToBeAdded`
+         */
+        public String concatenate(String baseValue, String valueToBeAdded) {
+            return baseValue + valueToBeAdded;
+        }
+    
+        /**
+         * @param valueToBeReversed value to be reversed
+         * @return identical string with characters in opposite order
+         */
+        public String reverse(String valueToBeReversed) {
+            String reversedValue = "";
+            String[] valueToBeReversedArr = valueToBeReversed.split("");
+            for(int i = 0; i < valueToBeReversedArr.length; i++) {
+                String letter = valueToBeReversedArr[valueToBeReversedArr.length-(i+1)];
+                String newLetter = valueToBeReversedArr[i];
+                newLetter = letter;
+                reversedValue += newLetter;
+                
+            }
+            
+            return reversedValue;
+        }
+    
+        /**
+         * @param word word to get middle character of
+         * @return middle character of `word`
+         */
+        public Character getMiddleCharacter(String word) {
+            String [] wordArr = word.split("");
+            int middle = wordArr.length/2; 
+            return word.charAt(middle) ;
+        }
+    
+        /**
+         * @param value value to have character removed from
+         * @param charToRemove character to be removed from `value`
+         * @return `value` with char of value `charToRemove` removed
+         */
+        public String removeCharacter(String value, Character charToRemove) {
+            char [] valueArr = value.toCharArray();
+            String noCharString = "";
+            for (int i = 0; i < valueArr.length; i++) {
+                if(valueArr[i] != charToRemove) {
+                    noCharString += valueArr[i];
+                }
+            }
+            return noCharString;
+        }
+    
+        /**
+         * @param sentence String delimited by spaces representative of a sentence
+         * @return last `word` in sentence
+         */
     public String getLastWord(String sentence) {
-        return null;
+        int x = sentence.lastIndexOf(' ');
+        char [] sentenceArr = sentence.toCharArray();
+        String lastWord = sentence.substring(x + 1,sentenceArr.length);
+        return lastWord;
     }
 }
