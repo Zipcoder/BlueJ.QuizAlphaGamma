320182eb64e5cd3094d49c39834bbc2d9ee48fef
Justin <justleland@gmail.com>
2019-05-28 09:46:33 -0400

    completed reverse method

--- StringUtilities.java
+++ StringUtilities.java
@@ -23,7 +23,11 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        String result = "";
+        for(int i = valueToBeReversed.length() - 1; i >= 0; i--) {
+            result += valueToBeReversed.charAt(i);
+        }
+        return result;
     }
 
     /**
