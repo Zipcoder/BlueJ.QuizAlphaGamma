dd1cacac606a5bf997aaad53ae24b1f80214d7d3
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-05-28 11:54:51 -0400

    updated tests

--- LoopsFunAcronymTest.java
+++ LoopsFunAcronymTest.java
@@ -51,6 +51,7 @@ public class LoopsFunAcronymTest {
         assertEquals(expected, actual);
     }
 
+    @Test
     public void test6() {
         // given
         String phrase = "Center Disease Control Whistle Blower";
@@ -62,7 +63,7 @@ public class LoopsFunAcronymTest {
         // then
         assertEquals(expected, actual);
     }
-
+    @Test
     public void test7() {
         // given
         String phrase = "Tuskegee Macon County, Alabama";
@@ -74,7 +75,7 @@ public class LoopsFunAcronymTest {
         // then
         assertEquals(expected, actual);
     }
-
+    @Test
     public void test8() {
         // given
         String phrase = "John Charles Cutler";
@@ -99,7 +100,7 @@ public class LoopsFunAcronymTest {
         // Then
         assertEquals(expected, actual);
     }
-
+    @Test
     public void test4() {
         // given
         String phrase = "Tactical Reconnaissance Nuclear Powered Aerospace Platform";
