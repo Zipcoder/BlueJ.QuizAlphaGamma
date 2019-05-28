8b8effd8aa5850ff92d9e58244fea1f70fae0adf
Abram <50758769+abramcpg@users.noreply.github.com>
2019-05-28 09:52:16 -0400

    Update MathUtilities.java

--- MathUtilities.java
+++ MathUtilities.java
@@ -8,8 +8,10 @@ public class MathUtilities{
    * @param valueToAdd number
    * @return the sum of the two numbers
    */
-  public Integer add(Integer baseValue, Integer valueToAdd){
-      return null;
+  public Integer add(Integer baseValue, Integer valueToAdd)
+  {
+      int answer = baseValue + valueToAdd;
+      return answer;
   }
 
   /**
@@ -18,8 +20,10 @@ public class MathUtilities{
    * @param valueToAdd second number
    * @return the sum of the two numbers
    */
-  public Double add(Double baseValue, Double valueToAdd){
-      return null;
+  public Double add(Double baseValue, Double valueToAdd)
+  {
+     Double answer = baseValue + valueToAdd;
+      return answer;
   }
 
   /**
@@ -27,8 +31,10 @@ public class MathUtilities{
    * @param number the number given
    * @return the half of the number in double
    */
-  public Double half(Integer number) {
-      return null;
+  public Double half(Integer number) 
+  {
+      Double answer = number / 2.0;
+      return answer;
   }
 
   /**
@@ -36,8 +42,18 @@ public class MathUtilities{
    * @param number the number given
    * @return true if the number is odd, false if it is even
    */
-  public Boolean isOdd(Integer number){
-      return null;
+  public Boolean isOdd(Integer number)
+  {
+      Boolean answer;
+      if ((number % 2) == (0)) //double couldn't be dereferenced when I used .equals()
+      {
+          answer = false; //even
+       }
+       else
+       {
+           answer = true; //odd
+        }
+      return answer;
   }
 
 
@@ -46,8 +62,10 @@ public class MathUtilities{
    * @param number the number given
    * @return the result of the number multiply by itself
    */
-  public Integer square(Integer number) {
-      return null;
+  public Integer square(Integer number) 
+  {
+      int answer = number * number;
+      return answer;
   }
 
 }
